020f450ca2441ad974f7deccee6297cf38d0887e
Hlabeli <hlabeli.zee@gmail.com>
2024-01-23 23:41:58 +0200

    Added databese configuration class

---
+++ src/main/java/com/thutohealthcare/medicalsystem/config/DatabaseConfig.java
@@ -0,0 +1,31 @@
+package com.thutohealthcare.medicalsystem.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+
+@Configuration
+@RequiredArgsConstructor
+public class DatabaseConfig {
+
+    private final Environment env;
+
+    public DatabaseConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName(Objects.requireNonNull(env.getProperty("spring.datasource.driver-class-name")))
+                .url(env.getProperty("spring.datasource.url"))
+                .username(env.getProperty("spring.datasource.username"))
+                .password(env.getProperty("spring.datasource.password"))
+                .build();
+    }
+}
