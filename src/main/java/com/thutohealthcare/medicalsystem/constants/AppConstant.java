c8a7e071eed6929e18bd12243ddaad02054480db
Hlabeli <hlabeli.zee@gmail.com>
2024-01-23 23:42:41 +0200

    Added AppConstant Interface

---
+++ src/main/java/com/thutohealthcare/medicalsystem/constants/AppConstant.java
@@ -0,0 +1,30 @@
+package com.thutohealthcare.medicalsystem.constants;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+public interface AppConstant {
+
+    public interface DateFormatters {
+        public SimpleDateFormat FORMATTER = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        public static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        public static List<Integer> AGES = List.of(18, 17, 20, 22, 19, 25, 30, 27, 12, 11, 45);
+    }
+
+    public interface DownloadFile {
+        public String ATTACHMENT_FILENAME = "";
+    }
+
+    public interface ResponseCode {
+        public String OK = "Success";
+        public String CREATED = "Success created";
+        public String UNAUTHORIZED = "You are not authorized";
+        public String UNKNOWN_STAFF = "Unknown staff trying to access the system. Please enter your UUID";
+    }
+
+    public interface Headers {
+        public String AUTHORIZATION = "Authorization";
+    }
+
+}
